9306ac054a12f5127c667cc70268e48b2d38efd7
falzzze <light.falze@gmail.com>
2023-03-17 04:19:52 +0300

    add components

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Учет сотрудников Департамента</h1>
+      <h2>Общее число сотрудников</h2>
+      <h2>Премию выписать следующим сотрудникам:</h2>
+    </div>
+  )
+}
+
+export default AppInfo;
